10f4fddcace9e9f2ab746c77338b1c6d8470fd97
liefesutliff <lsutliff@student.cvtc.edu>
2023-06-01 15:27:02 -0500

    added image gallery

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ function toggleAudioPlayer() {
   var audioPlayer = document.querySelector('.firstSongAudioPlayer');
   audioPlayer.classList.toggle('show');
 }
+
+function imageSlidshow() {
+  var image = document.querySelector('.profilePic');
+
+  if (image.src.includes("images/profilePic1.jpg")) {
+    image.src = "images/profilePic2.jpg"
+  } else if (image.src.includes("images/profilePic2.jpg")) {
+    image.src = "images/profilePic3.jpg"
+  } else {
+    image.src = "images/profilePic1.jpg"
+  }
+}
